11645eb84612c53c68df2d5bef6bed4a6b2b6f04
Emily Parkes <emilycoco@me.com>
2018-02-09 10:11:21 +1300

    create date component with basics

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      location: [],
+      activity: [],
+      gift: []
+    }
+  }
+  render () {
+    return (
+      <Date location={this.state.location} activity={this.state.activity} gift={this.state.gift} />
+    )
+  }
 }
 
 export default App
-
---
+++ client/components/Date.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Date extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: null
+    }
+  }
+  render () { 
+
+  }
+  return (
+    <h1>git commit to me!</h1>
+  )
+}
+
+export default Date 
