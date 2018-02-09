9ca19b8fc4a372a36bdce6aa8c8ef64ab77f6594
Kimmi Rindel <kimmi@Kimmis-MacBook-Pro.local>
2018-02-09 15:38:04 +1300

    added cheer sound

--- client/components/LoveResult.jsx
+++ client/components/LoveResult.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Sound from 'react-sound'
 
 const LoveResult = (props) => {
   return (
     <div className='body'>
+    <Sound url='cheer.wav' playStatus={Sound.status.PLAYING} />
       <h1>Your results:</h1>
       <h3 className='text'>{props.data.fname}</h3>
       <h2> and </h2>
       <h3 className='text'>{props.data.sname}</h3>
-      <h2>{props.data.percentage} % compatibility </h2>
+      <h2>{props.data.percentage} % chance of succesful date </h2>
       <h2>{props.data.result}</h2>
     </div>
   )
